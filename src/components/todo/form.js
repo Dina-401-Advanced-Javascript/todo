4610edf27dfc93112d95312d65b01ca54f7623a9
Dina Ayoub <dinaayoub@hotmail.com>
2021-01-18 14:03:03 -0800

    refactored everything into functions instead of classes

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,49 +1,46 @@
-import React from 'react';
+import React , {useState} from 'react';
 
-class TodoForm extends React.Component {
+function ToDoForm(props){
 
-  constructor(props) {
-    super(props);
-    this.state = { item: {} };
-  }
-  handleInputChange = e => {
-    this.setState({ item: {...this.state.item, [e.target.name]: e.target.value } });
-  };
-
-  handleSubmit = (e) => {
+  const [item, setItem] = useState('');
+  const handleInputChange = e => {
     e.preventDefault();
-    e.target.reset();
-    this.props.handleSubmit(this.state.item);
-    const item = {};
-    this.setState({item});
+    setItem({...item, [e.target.name]: e.target.value});
   };
 
-  render() {
-    return (
-      <>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (e.target.text.value) {
+      props.handleSubmit(item);
+      e.target.reset();
+      setItem({});
+    }
+  };
+
+  return(
+    <>
         <h3>Add Item</h3>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <label>
             <span>To Do Item</span>
             <input
               name="text"
               placeholder="Add To Do List Item"
-              onChange={this.handleInputChange}
+              onChange={handleInputChange}
             />
           </label>
           <label>
             <span>Difficulty Rating</span>
-            <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={this.handleInputChange} />
+            <input defaultValue="1" type="range" min="1" max="5" name="difficulty" onChange={handleInputChange} />
           </label>
           <label>
             <span>Assigned To</span>
-            <input type="text" name="assignee" placeholder="Assigned To" onChange={this.handleInputChange} />
+            <input type="text" name="assignee" placeholder="Assigned To" onChange={handleInputChange} />
           </label>
           <button>Add Item</button>
         </form>
       </>
-    );
-  }
+  );
 }
 
-export default TodoForm;
+export default ToDoForm;
